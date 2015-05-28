fd5a130f3e19e627b917e38d4f3a446dbec30ea7
Ben Norris <ben@bsn.io>
2015-05-28 10:05:23 -0600

    Sets up goal line as dashed single color line

--- JBChartViewDemo/JBChartViewDemo/Constants/JBColorConstants.h
+++ JBChartViewDemo/JBChartViewDemo/Constants/JBColorConstants.h
@@ -29,7 +29,7 @@
 #define kJBColorLineChartHeaderSeparatorColor UIColorFromHex(0x8eb6b7)
 #define kJBColorLineChartDefaultSolidLineColor [UIColor colorWithWhite:1.0 alpha:1.0]
 #define kJBColorLineChartDefaultSolidSelectedLineColor [UIColor colorWithWhite:1.0 alpha:1.0]
-#define kJBColorLineChartDefaultDashedLineColor [UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:0.5]
+#define kJBColorLineChartDefaultDashedLineColor [UIColor colorWithWhite:1.0 alpha:0.5]
 #define kJBColorLineChartDefaultDashedSelectedLineColor [UIColor colorWithWhite:1.0 alpha:1.0]
 
 #define mark - Area Chart
